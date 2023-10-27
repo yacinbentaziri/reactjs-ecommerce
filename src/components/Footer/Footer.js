e462c68ae6551bb65fd8aea81cc0beee9222de53
yacinbtz <yacin.bentaziri@gmail.com>
2023-10-28 00:03:18 +0200

    first commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ function Footer() {
                 <br />
                 <br />
 
-                <div id="credit"><a href="https://linkedin.com/in/priyanka-sharma-b79401142"> © Priyanka Sharma </a>&<a href="https://www.edyoda.com/" target="_blank"> EDYODA </a> | <a href="https://www.linkedin.com/in/yassine-ben-taziri-60a887257/" target="_blank"> YACINE BENTAZIRI </a></div>
+                <div id="credit"> <a href="https://www.linkedin.com/in/yassine-ben-taziri-60a887257/" target="_blank"> YACINE BENTAZIRI </a></div>
             </section>
 
         </footer>
